a163ebfb899f30f367e19eba74493b0df9001f97
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2023-08-11 15:56:16 -0300

    Feat: regra de Item extra não pode ser pedido sem o principal

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -12,19 +12,20 @@ const paymentMethods = ["dinheiro", "debito", "credito"];
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-      if(itens.length < 1) {
-        return messages["emptyCart"];
-      }
+      const onlyItemsNames = [];
 
-      if(!paymentMethods.includes(metodoDePagamento)) {
-        return messages["invalidPaymentMethod"];
-      }
+      if(itens.length < 1) return messages["emptyCart"];
+      if(!paymentMethods.includes(metodoDePagamento)) return messages["invalidPaymentMethod"];
 
       for(const item of itens) {
         const [itemName, quantity] = item.split(',');
-
+        
         if (!store.includes(itemName)) return messages["invalidItem"];
         if(parseInt(quantity) === 0) return messages["invalidQuantity"];
+        if(itemName === 'chantily' && !onlyItemsNames.includes('cafe')) return messages["extraWithoutMain"];
+        if(itemName === 'queijo' && !onlyItemsNames.includes('sanduiche')) return messages["extraWithoutMain"];
+
+        onlyItemsNames.push(itemName);
       }
 
         return "";
@@ -33,7 +34,7 @@ class CaixaDaLanchonete {
 }
 
 const resultado = new CaixaDaLanchonete()
-.calcularValorDaCompra('debito', ['pizza, 1']);
+.calcularValorDaCompra('dinheiro', ['cafe,1','chantily,1']);
 
 console.log(resultado);
 
