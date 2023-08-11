0a3c43dcfa4f67d8f843c38d68e27f83d6150169
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2023-08-11 14:13:30 -0300

    Feat: regra do carrinho vazio

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,25 @@
+const messages = {
+  emptyCart: "Não há itens no carrinho de compra!",
+  extraWithoutMain: "Item extra não pode ser pedido sem o principal",
+};
+
+const store = ["cafe", "chantily", "suco", "sanduiche", "queijo", "salgado", "combo1", "combo2"];
+
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
+      if(itens.length < 1) {
+        return messages["emptyCart"];
+      }
+
         return "";
     }
 
 }
+
+const resultado = new CaixaDaLanchonete()
+.calcularValorDaCompra('debito', []);
+
+console.log(resultado);
 
 export { CaixaDaLanchonete };
